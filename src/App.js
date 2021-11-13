674e83b8578abab22338b289ea65f626aa16b82e
Gzim <gzimasanni@gmail.com>
2021-11-13 12:11:16 +0100

    Add cart

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
-        < Meals />
+        <Meals />
       </main>
     </Fragment>
   );
